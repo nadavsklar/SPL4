1edae5ecf47d39bf066f2c1bb4315ee82dabd0cb
nadavsklar <sklar@post.bgu.ac.il>
2019-01-09 03:00:27 +0200

    working on create_db

--- src/create_db.py
+++ src/create_db.py
@@ -1,13 +1,13 @@
 import sys
 import sqlite3
+import os
 
 databaseName = "schedule.db"
 coursesTable = "courses"
 studentsTable = "students"
 classroomsTable = "classrooms"
-_conn = sqlite3.connect(databaseName)
 
-def createTables():
+def createTables(_conn):
     _conn.executescript("""
         CREATE TABLE students (
             grade      TEXT      PRIMARY KEY,
@@ -24,6 +24,7 @@ def createTables():
         CREATE TABLE courses (
             id                      INTEGER     PRIMARY KEY,
             course_name             TEXT        NOT NULL,
+            student                 TEXT        NOT NULL,
             number_of_students      INTEGER     NOT NULL,
             class_id                INTEGER     REFERENCES classrooms(id),
             course_length           INTEGER     NOT NULL
@@ -31,10 +32,44 @@ def createTables():
     """)
 
 
+def insert_to_students(details, _conn):
+    _conn.execute("""
+        INSERT INTO students (grade, count) VALUES (?, ?)
+    """, [details[1], details[2]])
+
+
+def insert_to_courses(details, _conn):
+    _conn.execute("""
+            INSERT INTO courses (id, course_name, student, number_of_students, class_id, course_length) 
+                VALUES (?, ?, ?, ?, ?, ?)
+        """, [details[1], details[2], details[3], details[4], details[5], details[6]])
+
+
+def insert_to_classrooms(details, _conn):
+    _conn.execute("""
+            INSERT INTO classrooms (id, location, current_course_id, current_course_time_left) VALUES (?, ?, 0, 0)
+        """, [details[1], details[2]])
+
+
+def initiate_tables_with_values(config_file_path, _conn):
+    file = open(config_file_path, "r")
+    for line in file:
+        line = line.strip()
+        details = line.split(', ')
+        if details[0] == 'S':
+            insert_to_students(details, _conn)
+        elif details[0] == 'R':
+            insert_to_classrooms(details, _conn)
+        elif details[0] == 'C':
+            insert_to_courses(details, _conn)
+
+
 def main(args):
-    # conn = sqlite3.connect(databaseName)
-    createTables()
-    print("hello")
+    if os.path.isfile(databaseName):
+        print("db exists")
+    _conn = sqlite3.connect(databaseName)
+#    createTables(_conn)
+    initiate_tables_with_values("/home/nadavsklar/PycharmProjects/SPL4/src/config1.txt", _conn)
 
 
 if __name__ == "__main__":
