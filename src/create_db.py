977329d2cd91128f5f7e3fca7bac2ae74e9670b8
nadavsklar <sklar@post.bgu.ac.il>
2019-01-09 02:05:01 +0200

    first files

--- src/create_db.py
+++ src/create_db.py
@@ -5,15 +5,36 @@ databaseName = "schedule.db"
 coursesTable = "courses"
 studentsTable = "students"
 classroomsTable = "classrooms"
-
+_conn = sqlite3.connect(databaseName)
 
 def createTables():
+    _conn.executescript("""
+        CREATE TABLE students (
+            grade      TEXT      PRIMARY KEY,
+            count      INTEGER   NOT NULL
+        );
 
-    return
+        CREATE TABLE classrooms (
+            id                          INTEGER         PRIMARY KEY,
+            location                    TEXT            NOT NULL,
+            current_course_id           INTEGER         NOT NULL,
+            current_course_time_left    INTEGER         NOT NULL
+        );
+
+        CREATE TABLE courses (
+            id                      INTEGER     PRIMARY KEY,
+            course_name             TEXT        NOT NULL,
+            number_of_students      INTEGER     NOT NULL,
+            class_id                INTEGER     REFERENCES classrooms(id),
+            course_length           INTEGER     NOT NULL
+        );
+    """)
 
 
 def main(args):
-    conn = sqlite3.connect(databaseName)
+    # conn = sqlite3.connect(databaseName)
+    createTables()
+    print("hello")
 
 
 if __name__ == "__main__":
